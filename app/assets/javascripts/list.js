918e10a75aa6dca464988d588d31d7d708c8c6e9
alexi215 <alexi83@gmail.com>
2014-10-15 19:12:14 -0400

    Increased Google Places API search radius to 5,000m; cleaned up code

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -4,57 +4,42 @@ $( document ).ready(function() {
 });
 
 var initialize = function() {
-  // console.log("initializing firing");
-
   autocomplete = new google.maps.places.Autocomplete(
     /** @type {HTMLInputElement} */(document.getElementById('autocomplete')),
       { types: ['geocode'] });
    
   google.maps.event.addListener(autocomplete, 'place_changed', function() {
     plainAddress = autocomplete.getPlace().formatted_address;
-      // console.log('Address selected by user: ' + plainAddress);
-    
     latLong = autocomplete.getPlace().geometry.location;
-      // console.log(latLong);
     
     var request = {
       location: latLong,
-      radius: 500,
+      radius: 5000,
       keyword: 'coffee shop'
     };
 
-    // console.log(request + " firing");
     placesList = document.getElementById('places');
-
-
     var service = new google.maps.places.PlacesService(placesList);
-    
     service.nearbySearch(request, callBack);
   });
 };
 
 function callBack(results, status) {
-  // console.log('callBack FIRING');
   if (status != google.maps.places.PlacesServiceStatus.OK) {
     alert(status);
     return;
   } else {
-    console.log(results);
     createListOfCoffeeShops(results);
   }
 }
 
 function createListOfCoffeeShops(shops) {
-  // console.log('createListOfCoffeeShops FIRING');
-  // console.log(shops);
-  
   var limit;
 
   if (shops.length > 10) {
     limit = 10;
   } else {
     limit = shops.length;
-    console.log(limit);
   }
   
   for (var i = 0; i < limit; i++) {
